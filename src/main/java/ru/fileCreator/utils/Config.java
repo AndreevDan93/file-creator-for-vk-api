6e5ef8603bf33abc48d911879366524f88746714
AndreevDan93 <andreevdan93@gmail.com>
2023-07-22 20:06:54 +0500

    fix jar bugs

--- src/main/java/ru/fileCreator/utils/Config.java
+++ src/main/java/ru/fileCreator/utils/Config.java
@@ -1,14 +1,28 @@
 package ru.fileCreator.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
+    private static final String RELATIVE_PATH_TO_PROPERTIES = "config.properties";
     private static Properties properties;
 
     public static String get(String key) {
         if (properties == null) {
-            properties = FileUtils.loadProperties("src/main/resources/config.properties");
-            return properties.getProperty(key);
+
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            InputStream input = classLoader.getResourceAsStream(RELATIVE_PATH_TO_PROPERTIES);
+//            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+//            InputStream input = classLoader.getResourceAsStream("");
+            try {
+                properties = new Properties();
+                properties.load(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
         return properties.getProperty(key);
     }
